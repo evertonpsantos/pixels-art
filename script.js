7c894ddf6b15e8ee1dd94acdf301b8e67c0cf298
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-25 16:22:22 -0300

    Requisitos 1 a 5

---
+++ script.js
@@ -0,0 +1,11 @@
+// function createPixelBoard(numero) {
+//    for (let i = 0; i < 5; i += 1) {
+//       const pixelBoard = document.getElementById('pixel-board');
+//       const blankSquare = document.createElement('li');
+//       blankSquare.className = 'pixel';
+//       blankSquare.style.backgroundColor = 'white';
+//       pixelBoard.appendChild(blankSquare);
+//    }
+// }
+
+// createPixelBoard();
