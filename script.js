71d05736d4cfef24141f894520f00ced568de4b4
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-25 18:12:40 -0300

    Requisitos 6 a 9

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-// function createPixelBoard(numero) {
-//    for (let i = 0; i < 5; i += 1) {
-//       const pixelBoard = document.getElementById('pixel-board');
-//       const blankSquare = document.createElement('li');
-//       blankSquare.className = 'pixel';
-//       blankSquare.style.backgroundColor = 'white';
-//       pixelBoard.appendChild(blankSquare);
-//    }
-// }
+const blackColor = document.getElementById('black-color');
+const redColor = document.getElementById('red-color');
+const blueColor = document.getElementById('blue-color');
+const greenColor = document.getElementById('green-color');
 
-// createPixelBoard();
+function selectColor(event) {
+  const previousSelectedElement = document.querySelector('.selected');
+  previousSelectedElement.style.borderColor = null;
+  previousSelectedElement.classList.remove('selected');
+  event.target.classList.add('selected');
+  event.target.style.borderColor = 'gold';
+}
+
+blackColor.addEventListener('click', selectColor);
+redColor.addEventListener('click', selectColor);
+blueColor.addEventListener('click', selectColor);
+greenColor.addEventListener('click', selectColor);
+
+function colorPixel(event) {
+   const selectedColor = document.querySelector('.selected');
+   const cssObj = window.getComputedStyle(selectedColor);
+   const bgColor = cssObj.getPropertyValue('background-color');
+   event.target.style.backgroundColor = bgColor;
+}
+
+const pixelsBrancos = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixelsBrancos.length; i += 1) {
+   pixelsBrancos[i].addEventListener('click', colorPixel);
+}
+
+function clearBoard() {
+   for (let i = 0; i < pixelsBrancos.length; i += 1) {
+      pixelsBrancos[i].style.backgroundColor = 'white';
+   }
+}
+
+const clearButton = document.getElementById('clear-board');
+clearButton.addEventListener('click', clearBoard);
